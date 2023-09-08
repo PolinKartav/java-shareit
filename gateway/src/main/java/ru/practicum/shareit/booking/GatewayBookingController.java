0a172b445ae64ec7c9de54ad522e08b4d21d35ff
PolinKartav <polyakarpushina@yandex.ru>
2023-09-08 20:26:33 +0300

    separated method;

--- gateway/src/main/java/ru/practicum/shareit/booking/GatewayBookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/GatewayBookingController.java
@@ -4,14 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.dto.CreateUpdateBookingDto;
 import ru.practicum.shareit.validator.ValuesAllowedConstraint;
 
@@ -30,7 +23,7 @@ public class GatewayBookingController {
 
     @PostMapping
     public ResponseEntity<Object> createBooking(@RequestHeader(REQUEST_HEADER_USER_ID) Long userId,
-                                    @Valid @RequestBody CreateUpdateBookingDto createUpdateBookingDto) {
+                                                @Valid @RequestBody CreateUpdateBookingDto createUpdateBookingDto) {
         return client.create(userId, createUpdateBookingDto);
     }
 
@@ -38,42 +31,43 @@ public class GatewayBookingController {
     public ResponseEntity<Object> getById(@RequestHeader(REQUEST_HEADER_USER_ID) Long userId, @PathVariable Long bookingId) {
         return client.getBookingByUserOwner(userId, bookingId);
     }
+
     @PatchMapping("/{bookingId}")
-    public ResponseEntity<Object>  confirmedBooking(@RequestHeader(REQUEST_HEADER_USER_ID) Long userId,
-                                       @PathVariable Long bookingId,
-                                       @RequestParam boolean approved) {
+    public ResponseEntity<Object> confirmedBooking(@RequestHeader(REQUEST_HEADER_USER_ID) Long userId,
+                                                   @PathVariable Long bookingId,
+                                                   @RequestParam boolean approved) {
         return client.approveBooking(userId, bookingId, approved);
     }
 
     @GetMapping
     public ResponseEntity<Object> getBookingsOfUser(@RequestHeader(REQUEST_HEADER_USER_ID) Long userId,
-                                              @ValuesAllowedConstraint(propName = "state",
-                                                      values = {"all",
-                                                              "current",
-                                                              "past",
-                                                              "future",
-                                                              "waiting",
-                                                              "rejected"},
-                                                      message = "Unknown state: UNSUPPORTED_STATUS")
-                                              @RequestParam(defaultValue = "all") String state,
-                                              @RequestParam(defaultValue = "0") @PositiveOrZero int from,
-                                              @RequestParam(defaultValue = "20") @Positive int size) {
+                                                    @ValuesAllowedConstraint(propName = "state",
+                                                            values = {"all",
+                                                                    "current",
+                                                                    "past",
+                                                                    "future",
+                                                                    "waiting",
+                                                                    "rejected"},
+                                                            message = "Unknown state: UNSUPPORTED_STATUS")
+                                                    @RequestParam(defaultValue = "all") String state,
+                                                    @RequestParam(defaultValue = "0") @PositiveOrZero int from,
+                                                    @RequestParam(defaultValue = "20") @Positive int size) {
         return client.getUserBookings(userId, String.valueOf(State.valueOf(state.toUpperCase())), from, size);
     }
 
     @GetMapping("/owner")
     public ResponseEntity<Object> getBookingsOfOwner(@RequestHeader(REQUEST_HEADER_USER_ID) Long userId,
-                                               @ValuesAllowedConstraint(propName = "state",
-                                                       values = {"all",
-                                                               "current",
-                                                               "past",
-                                                               "future",
-                                                               "waiting",
-                                                               "rejected"},
-                                                       message = "Unknown state: UNSUPPORTED_STATUS")
-                                               @RequestParam(defaultValue = "all") String state,
-                                               @RequestParam(defaultValue = "0") @PositiveOrZero int from,
-                                               @RequestParam(defaultValue = "20") @Positive int size) {
+                                                     @ValuesAllowedConstraint(propName = "state",
+                                                             values = {"all",
+                                                                     "current",
+                                                                     "past",
+                                                                     "future",
+                                                                     "waiting",
+                                                                     "rejected"},
+                                                             message = "Unknown state: UNSUPPORTED_STATUS")
+                                                     @RequestParam(defaultValue = "all") String state,
+                                                     @RequestParam(defaultValue = "0") @PositiveOrZero int from,
+                                                     @RequestParam(defaultValue = "20") @Positive int size) {
         return client.getOwnerBookings(userId, String.valueOf(State.valueOf(state.toUpperCase())), from, size);
     }
 
